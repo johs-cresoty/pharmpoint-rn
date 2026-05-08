bf5d5177a41031e21b366025c36b95aedfdbe794
johs <johs@cresoty.co.kr>
2026-05-08 13:18:12 +0900

    init

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        alias: {
+          '@features': './src/features',
+          '@shared': './src/shared',
+          '@native': './src/native',
+          '@navigation': './src/navigation',
+        },
+      },
+    ],
+  ],
 };
